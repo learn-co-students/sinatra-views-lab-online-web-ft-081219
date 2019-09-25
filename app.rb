a359491af4f6fd799d40acfccba3f18a0365589f
Cloe Kouadjo <armelfli50@gmail.com>
2019-09-25 00:14:25 +0000

    Done.

--- app.rb
+++ app.rb
@@ -4,5 +4,16 @@ class App < Sinatra::Base
 		erb :index
 	end
 
+get '/hello' do
+		erb :hello
+	end
+get '/goodbye' do 
+  @bye = "Goodbye Joe"
+  erb :goodbye
+end
 
+get '/date' do
+  @date = "Wednesday, September 25, 2019"
+  erb :date
+end
 end
